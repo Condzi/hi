412e89b760e8864f3564e3a14ac1c0acc487222d
Konrad Kubacki <Condzi@users.noreply.github.com>
2024-02-03 23:06:17 +0100

    Disable more useless warnings

--- analyze.bat
+++ analyze.bat
@@ -22,16 +22,16 @@ if not "%web%"=="1" set web=0
 set cc_dir=         .codechecker
 set cc_report_dir=   %cc_dir%/report/
 set cc_http_dir=     %cc_dir%/report_http/
-set cc_analyze=      call CodeChecker analyze
+set cc_analyze=      call CodeChecker analyze --config codechecker.yml
 set cc_parse=        call CodeChecker parse
 set cc_compile_cmds= compile_commands.json
-set cc_analyzers=    clangsa clang-tidy cppcheck
+set cc_analyzers=    clangsa clang-tidy
 
 if not exist %cc_dir% mkdir %cc_dir%
 if not exist %cc_report_dir% set new=1
 
 :: --- Run the analysis -------------------------------------------------------
-if "%new%"=="1" %cc_analyze% -c -i skipfile.txt --output %cc_report_dir% %cc_compile_cmds% --analyzers %cc_analyzers% --analyzer-config 'clang-tidy:take-config-from-directory=true'
+if "%new%"=="1" %cc_analyze% -c -i skipfile.txt --output %cc_report_dir% %cc_compile_cmds% --analyzers %cc_analyzers% --analyzer-config 'clang-tidy:take-config-from-directory=true' --analyzer-config clangsa:AllEnable=true
 
 :: --- Generate the report ----------------------------------------------------
 if "%web%"=="1" %cc_parse% -e html %cc_report_dir% -o %cc_http_dir%
