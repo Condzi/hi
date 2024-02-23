a359172fd9dbf52f4356c21e91adeac0c801e1e9
Konrad Kubacki <Condzi@users.noreply.github.com>
2024-02-23 22:43:29 +0100

    base: block allocator ideas based on box2c by Erin Catto

--- code/base/block_allocator.cpp
+++ code/base/block_allocator.cpp
@@ -1,2 +1,68 @@
 #pragma once
 #include "all_inc.hpp"
+
+// Original code by Erin Catto, Box2c. I just removed dynamic allocation in favor of static
+// arena-like buffer.
+
+internal read_only u64 BLOCK_SIZES[] = {
+    16,  // 0
+    32,  // 1
+    64,  // 2
+    96,  // 3
+    128, // 4
+    160, // 5
+    192, // 6
+    224, // 7
+    256, // 8
+    320, // 9
+    384, // 10
+    448, // 11
+    512, // 12
+    640, // 13
+};
+
+internal read_only u64 BLOCK_SIZES_COUNT = ArrayCount(BLOCK_SIZES);
+internal read_only u64 BLOCK_SIZE_MAX    = BLOCK_SIZES[BLOCK_SIZES_COUNT - 1];
+
+struct Block_Size_Map {
+  u8 values[BLOCK_SIZE_MAX + 1];
+};
+
+consteval Block_Size_Map
+generate_block_size_map() {
+  Block_Size_Map map = {};
+  map.values[0]      = 0;
+  s32 j              = 0;
+  for (int32_t i = 1; i <= BLOCK_SIZE_MAX; ++i) {
+    if (i <= BLOCK_SIZES[j]) {
+      map.values[i] = (uint8_t)j;
+    } else {
+      ++j;
+      map.values[i] = (uint8_t)j;
+    }
+  }
+  return map;
+}
+
+internal read_only Block_Size_Map BLOCK_SIZE_MAP = generate_block_size_map();
+
+struct Memory_Block {
+  Memory_Block *next;
+  Memory_Block *prev;
+  u64           base_pos;
+};
+
+struct Memory_Chunk {
+  Memory_Block *next;
+  u64           sz;
+  Memory_Block *blocks;
+};
+
+struct Block_Allocator {
+  Memory_Chunk *used;
+  Memory_Chunk *free;
+
+  u64 base_pos;
+  u64 curr_pos;
+  u64 reserved;
+};
--- code/base/block_allocator.hpp
+++ code/base/block_allocator.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+// Internally holds multiple lists of blocks. blocks have few common sizes: 16, 32, 64, 96...512.
+// On allocation, we just take one of free blocks and return it.
+// It's a more sophisticated version of Arena allocator, where you can release the allocation
+// without the need of releasing the entire allocator.
+//
+struct Block_Allocator;
+
+must_use global Block_Allocator *
+make_block_allocator(u64 base, u64 sz);
+
+global void
+unmake_block_allocator(Block_Allocator *allocator);
+
+must_use global void *
+alloc_block(Block_Allocator *allocator, u64 sz);
+
+global void
+free_block(Block_Allocator *allocator, void *block);
