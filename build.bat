2c43e6b005dcb0c823a0e91bebcf75e735bb78a2
Konrad Kubacki <Condzi@users.noreply.github.com>
2024-02-29 22:41:44 +0100

    build: add UBSAN

--- build.bat
+++ build.bat
@@ -34,6 +34,7 @@ if not "%ERRORLEVEL%" == "0" (
 :: Below is a list of all possible non-target command line options:
 ::
 :: - `asan`: enable address sanitizer
+:: - `ubsan`: enable undefined behavior sanitizer
 :: - `msvc`: compile with MSVC (needs x64 native tools cmd prompt)
 :: - `clang`: compile with clang++
 
@@ -51,10 +52,13 @@ if "%~1"==""        echo [default mode, assuming `game` build] && set game=1
 :: --- Unpack Command Line Build Arguments ------------------------------------
 if "%asan%"=="1"    echo [asan enabled]
 
-set msvc_asan=
-set clang_asan=
-if "%asan%"=="1"      set msvc_asan= -fsanitize=address
-if "%asan%"=="1"      set clang_asan=-fsanitize=address -fno-omit-frame-pointer
+set msvc_sanitizer=
+set clang_sanitizer=
+if "%asan%"=="1"      set msvc_sanitizer= -fsanitize=address 
+if "%asan%"=="1"      set clang_sanitizer=-fsanitize=address -fno-omit-frame-pointer
+
+if "%ubsan%"=="1"     set msvc_sanitizer= -fsanitize=undefined
+if "%ubsan%"=="1"     set clang_sanitizer=-fsanitize=undefined -fno-omit-frame-pointer
 
 
 :: --- MSVC  -----------------------------------------------------------
@@ -77,7 +81,7 @@ if "%asan%"=="1"      set clang_asan=-fsanitize=address -fno-omit-frame-pointer
 set msvc_defines=           /DWIN32 /D_WINDOWS /D_HAS_EXCEPTIONS=0 /D_CRT_SECURE_NO_WARNINGS
 set msvc_disabled_warnings= /wd4505 /wd5045 /wd4577 /wd4820 /wd4201 /wd5246 /wd4710 /wd4711 /wd4061
 set msvc_misc=              /GR- /Wall /WX /external:anglebrackets /external:W0 /GS /sdl /utf-8
-set msvc_all=               %msvc_asan% %msvc_defines% %msvc_disabled_warnings% %msvc_misc%
+set msvc_all=               %msvc_sanitizer% %msvc_defines% %msvc_disabled_warnings% %msvc_misc%
 
 :: --- Compile/Link Line Definitions ------------------------------------------
 set cl_common=        /I..\code\ /I..\code\3rdparty\ /nologo /FC /Z7 /std:c++20
@@ -92,11 +96,13 @@ set clang_disabled_warnings= -Wno-format -Wno-pragma-once-outside-header -Wno-gc
 set clang_defines=    -DWIN32 -D_WINDOWS -D_HAS_EXCEPTIONS=0 -D_CRT_SECURE_NO_WARNING
 set clang_misc=       -fno-exceptions -fno-rtti -ferror-limit=0 -Wall -Wextra -Werror
 set clang_common=     -I..\code\ -I..\code\3rdparty -std=c++20 %clang_defines% %clang_misc% %clang_disabled_warnings%
-set clang_debug=      call clang++ -g -O0 %clang_common% %clang_asan%
+set clang_debug=      call clang++ -g -O0 %clang_common% %clang_sanitizer%
 set clang_release=    call clang++ -Ofast -DNDEBUG %clang_common%
 set clang_out=         -o 
-set clang_link=       
+set clang_link=        -fuse-ld=lld-link -Wl,/MANIFEST:EMBED,/INCREMENTAL:NO
 
+:: This is bullshit workaround for broken UBSAN on windows.
+if "%ubsan%"=="1"      set clang_link=%clang_link%,"C:\Program Files\LLVM\lib\clang\18\lib\windows\clang_rt.ubsan_standalone-x86_64.lib","C:\Program Files\LLVM\lib\clang\18\lib\windows\clang_rt.ubsan_standalone_cxx-x86_64.lib"
 :: --- Choose Compile/Link Lines ----------------------------------------------
 set compile_commands=
 
